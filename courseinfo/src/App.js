4a935c908079a3720c6ca7b6282d4d192caf0cf8
Vrinda VJ <vrinda@Vrindas-MacBook-Pro.local>
2022-07-23 22:46:04 +0300

    exercise 1.2 added

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -3,20 +3,19 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
+// Part Component
+const Part = (props) => {
+  return <p>{props.part} {props.exercise} </p>;
+};
+
 // Content Component
 const Content = (props) => {
   console.log(props);
   return (
     <div>
-      <p>
-        {props.parts[0]} {props.exercise[0]}
-      </p>
-      <p>
-        {props.parts[1]} {props.exercise[1]}
-      </p>
-      <p>
-        {props.parts[2]} {props.exercise[2]}
-      </p>
+      <Part part={props.parts[0]} exercise= {props.exercises[0]}/>
+      <Part part={props.parts[1]} exercise= {props.exercises[1]}/>
+      <Part part={props.parts[2]} exercise= {props.exercises[2]}/>
     </div>
   );
 };
@@ -40,7 +39,7 @@ const App = () => {
       <Header course={course} />
       <Content
         parts={[part1, part2, part3]}
-        exercise={[exercises1, exercises2, exercises3]}
+        exercises={[exercises1, exercises2, exercises3]}
       />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
