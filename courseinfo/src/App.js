66be5cca38b7d67fd77d52366d3bdaa4fd5575d5
Vrinda VJ <vrinda@Vrindas-MacBook-Pro.local>
2022-07-23 22:34:55 +0300

    exercise 1.1 added

---
+++ courseinfo/src/App.js
@@ -0,0 +1,50 @@
+// Header Component
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+// Content Component
+const Content = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        {props.parts[0]} {props.exercise[0]}
+      </p>
+      <p>
+        {props.parts[1]} {props.exercise[1]}
+      </p>
+      <p>
+        {props.parts[2]} {props.exercise[2]}
+      </p>
+    </div>
+  );
+};
+
+// Total Component
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        parts={[part1, part2, part3]}
+        exercise={[exercises1, exercises2, exercises3]}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+export default App;
